e994da5535468e2de45368814fe316fd820be9bd
Thomas Ruecroft <thomas.ruecroft1@nhs.net>
2020-01-21 18:33:31 +0000

    Cleaned up main a little

--- exercises/src/week2/library/client/Main.java
+++ exercises/src/week2/library/client/Main.java
@@ -8,25 +8,24 @@ import week2.library.model.*;
 public class Main {
 
 	public static void main(String[] args) {
-		// Create 5 different books to add to the books array in the model
-		String[] book1 = new String[] {"Book 1", "a1", "a2", "a3", "a4", "a5", "5"};
-		String[] book2 = new String[] {"Book 2", "b1", "b2", "b3", "b4", "b5", "10"};
-		String[] book3 = new String[] {"Book 3", "c1", "c2", "c3", "c4", "c5", "15"};
-		String[] book4 = new String[] {"Book 4", "d1", "d2", "d3", "d4", "d5", "20"};
-		String[] book5 = new String[] {"Book 5", "e1", "e2", "e3", "e4", "e5", "25"};
+		addBooks();
+		outputBooks();
+	}
+	public static void addBooks() {
 		// Add each book to the books array
-		Book.addBook(book1);
-		Book.addBook(book2);
-		Book.addBook(book3);
-		Book.addBook(book4);
-		Book.addBook(book5);
+		Book.addBook(new String[] {"Book 1", "a1", "a2", "a3", "a4", "a5", "5"});
+		Book.addBook(new String[] {"Book 2", "b1", "b2", "b3", "b4", "b5", "10"});
+		Book.addBook(new String[] {"Book 3", "c1", "c2", "c3", "c4", "c5", "15"});
+		Book.addBook(new String[] {"Book 4", "d1", "d2", "d3", "d4", "d5", "20"});
+		Book.addBook(new String[] {"Book 5", "e1", "e2", "e3", "e4", "e5", "25"});
+	}
+	public static void outputBooks() {
 		// Get all books from the model
 		ArrayList<String[]> books = Book.getBooks();
 		// Output each book from the books model
 		for (String[] book: books) {
 			System.out.println("Book: " + Arrays.toString(book));
-		}	
-		
+		}
 	}
 
 }

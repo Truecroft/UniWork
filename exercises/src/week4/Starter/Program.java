9972bcb8071f1d2133a897286b13d581201ee895
Thomas Ruecroft <thomas.ruecroft1@nhs.net>
2020-01-25 21:53:11 +0000

    Start of Week 4

---
+++ exercises/src/week4/Starter/Program.java
@@ -0,0 +1,26 @@
+package week4.Starter;
+
+
+import java.awt.Color;
+import java.awt.Point;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+
+		System.out.print("\nShape Characteristics\n");
+		System.out.print("----------------------\n");
+ 		//System.out.printf("Colour:        %s\n", blob.getColour());
+ 		//System.out.printf("Position:      %s\n", blob.getPosition());
+
+		System.out.print("\nCircle Characteristics\n");
+		System.out.print("----------------------\n");
+ 		//System.out.printf("Colour:        %s\n", hoop.getColour());
+ 		//System.out.printf("Position:      %s\n", hoop.getPosition());
+ 		
+ 		System.out.println();
+
+	}
+
+}
